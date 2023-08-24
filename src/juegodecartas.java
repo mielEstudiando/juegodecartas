0f1ec61ddb2a37d0eb95edea9590c1d0d87d3e67
miel <m.ferrada09@ufromail.cl>
2023-08-24 15:53:45 -0400

    Tercer commit, proceso + pruebas y algunas correcciones

--- src/juegodecartas.java
+++ src/juegodecartas.java
@@ -1,23 +1,52 @@
+import java.util.Random;
 public class juegodecartas {
-    public static void crearMatrizCartas(){
-        String[][] MatrizCartas = new String[13][4];
+    public static String[][] crearMatrizCartas(){
+        return new String[13][4];
     }
 
-    public static void agregarCartas(String[][] Matriz, String carta, String puntaje, int p, int c){
-        Matriz[p][c]= carta + ", " + puntaje;
+    public static void agregarCartas(String[][] Matriz,
+                                     String carta, String puntaje,
+                                     int f, int c){
+        Matriz[f][c]= carta + ", " + puntaje;
     }
 
-    public static void inicializarCartasJuego(){
-        String[] simbolos = new String[] {"Corazones", "Diamantes", "Picas", "Tréboles"};
-        String[] numeros = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"};
-        crearMatrizCartas();
+    public static String[][] inicializarCartasJuego(){
+        String[] simbolos = new String[] {"Corazones",
+                "Diamantes", "Picas", "Tréboles"};
+        String[] numeros = new String[] {"1", "2",
+                "3", "4", "5", "6", "7", "8", "9",
+                "J", "Q", "K", "A"};
+        String[] puntajeC = new String[] {"1", "2",
+                "3", "4", "5", "6", "7", "8", "9",
+                "10", "10", "10", "11"};
+        String[][] MatrizCartas = crearMatrizCartas();
 
-        for (int i=0;i<13;i++){
-            for (int j=0; j<4; j++){
+        for (int fila=0;fila<13;fila++){
+            for (int colum=0; colum<4; colum++){
 
-                MatrizCartas[][]
+                agregarCartas(MatrizCartas,
+                        numeros[fila] + " de " + simbolos[colum],
+                        puntajeC[fila], fila, colum);
             }
         }
+        return MatrizCartas;
+    }
 
+    public static String[] obtenerCartas(String[][] matriz){
+        String[] cartas = new String[3];
+        for (int i=0; i<3; i++){
+            Random random= new Random();
+            int cartaA= random.nextInt(13);
+            int cartaB= random.nextInt(4);
+            cartas[i]=matriz[cartaA][cartaB];
+        }
+        return cartas;
+    }
+
+    public static void main(String[] args){
+        String[][] matrizCartas =inicializarCartasJuego();
+        Random random= new Random();
+        int cartaA= random.nextInt(2);
+        System.out.println(cartaA);
     }
 }
